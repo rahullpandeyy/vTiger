d39ece9e292ef7d202a7c064307ec0e687a5ab60
RAHUL PANDEY <you@example.com>
2024-03-30 16:53:32 +0530

    created framework a library

---
+++ vTiger/src/main/java/PomRepository/CampaignsPage.java
@@ -0,0 +1,28 @@
+package PomRepository;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CampaignsPage {
+	public CampaignsPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//img[@title=\"Create Campaign...\"]")
+	private WebElement addCampaignPlusButton;
+
+	
+	
+	public WebElement getAddCampaignPlusButton() {
+		return addCampaignPlusButton;
+	}
+
+
+
+	public void clickOnCampaignPlusButton() {
+		addCampaignPlusButton.click();
+	}
+}
---
+++ vTiger/src/main/java/PomRepository/LeadsPage.java
@@ -0,0 +1,26 @@
+package PomRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LeadsPage {
+
+	public LeadsPage(WebDriver driver) {
+
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//img[@alt=\"Create Lead...\"]")
+	private WebElement addLeadsPlusButton;
+
+	public WebElement getAddLeadsPlusButton() {
+		return addLeadsPlusButton;
+	}
+	
+	public void clickOnLeadsPlusButton() {
+		addLeadsPlusButton.click();
+	}
+
+}
