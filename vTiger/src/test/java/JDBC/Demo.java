3eb8a5ff1a8b03a22e1d8706266bd2215f7890fd
rahullpandeyy <154499959+rahullpandeyy@users.noreply.github.com>
2024-03-30 22:59:13 +0530

    Update Demo.java

--- vTiger/src/test/java/JDBC/Demo.java
+++ vTiger/src/test/java/JDBC/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("push branch 1");
-		System.out.println("pull branch 1");
+		System.out.println("pull branch 2");
+		System.out.println("push branch 3");
 		// TODO Auto-generated method stub
 
 	}
